33002ef8e69304703885b6cb8ffd9507fc362c7d
Max Hardtke <Max-Tom@outlook.de>
2022-06-18 14:08:45 +0200

    ~ switched to SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import type {NextPage} from 'next';
 import React, {ReactNode, useEffect, useState} from "react";
 import Layout from "../components/Layout";
-import {getFrontendMonitorData, MonitorData, MonitorStats} from "../util/cronitorUtils";
+import {getFrontendMonitorData, getMonitorData, MonitorData, MonitorStats} from "../util/cronitorUtils";
 import moment from "moment";
 
-const Home: NextPage = () => {
+const Home = (props : {monitorStats: MonitorStats, error: string}) => {
 
-  const [monitorStats, setMonitorStats] = useState<MonitorStats>();
-  const [loadingError, setLoadingError] = useState<string>();
+  const [monitorStats, setMonitorStats] = useState<MonitorStats>(props.monitorStats);
+  const [loadingError, setLoadingError] = useState<string | undefined>(props.error);
 
   useEffect(() => {
     getFrontendMonitorData().then(res => {
       setMonitorStats(res);
+      setLoadingError(undefined);
     }).catch(err => {
-      setLoadingError(err.message);
+      //setLoadingError(err.message);
     })
   }, []);
 
@@ -28,11 +28,11 @@ const Home: NextPage = () => {
   const StatsColum = (props : {children: ReactNode, title: ReactNode, footer: ReactNode}) => {
 
     return (
-      <div className="py-4 md:px-4 flex-grow-0">
+      <div className="py-4 md:px-10 flex-grow-0">
         <div className="flex-grow-0 text-center md:text-start">
-          <div className="text-gray-500 font-light uppercase text-xs mb-2">{props.title}</div>
-          {props.children}
-          <div className={"text-gray-500 font-light text-xs mt-2"}>
+          <div className="text-gray-400 font-light uppercase text-md mb-2">{props.title}</div>
+          <div className="text-lg">{props.children}</div>
+          <div className={"text-gray-400 font-light text-sm mt-2"}>
             {props.footer}
           </div>
         </div>
@@ -51,13 +51,13 @@ const Home: NextPage = () => {
             <>
               {monitor.status === "HEALTHY" &&
                   <span className={"bg-green-800 text-white py-0.5 px-3 rounded-md flex-grow-0"}>
-                        Healthy
-                      </span>
+                    Healthy
+                  </span>
               }
               {monitor.status !== "HEALTHY" &&
                   <span className={"bg-red-800 text-white py-0.5 px-3 rounded-md flex-grow-0"}>
-                        Failing
-                      </span>
+                    Failing
+                  </span>
               }
             </>
           </StatsColum>
@@ -90,7 +90,7 @@ const Home: NextPage = () => {
       <div className={"container mx-auto flex min-h-screen"}>
         <div className="pt-10 mx-auto text-white">
 
-          {!monitorStats && <div className={"h-full flex"}><div className="my-auto loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-64 w-64"/></div>}
+          {(!monitorStats && !loadingError) && <div className={"h-full flex"}><div className="my-auto loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-64 w-64"/></div>}
 
           {monitorStats &&(
             <div className={""}>
@@ -113,12 +113,15 @@ const Home: NextPage = () => {
           {loadingError &&
             <div className={"mt-20"}>
                 <div className={"bg-red-700 bg-opacity-50 rounded-md px-5 py-3"}>
-                    <h2 className={"text-2xl"}>Error:</h2>
-                    <div className={"h-px bg-gray-600"}/>
-                    <div className="">
-                        Leider ist beim laden der Statusseite ein Fehler aufgetreten.
-                        Bitte versuch es erneut.
-                    </div>
+                  <h2 className={"text-2xl"}>Error:</h2>
+                  <div className={"h-px bg-gray-600"}/>
+                  <div className="">
+                      Leider ist beim laden der Statusseite ein Fehler aufgetreten.
+                      Bitte versuch es erneut.
+                  </div>
+                  <div className={"italic text-gray-400"}>
+                    {loadingError}
+                  </div>
                 </div>
             </div>
           }
@@ -127,6 +130,27 @@ const Home: NextPage = () => {
       </div>
     </Layout>
   )
+}
+
+export const getStaticProps = async () => {
+
+  try {
+    const monitorStats = await getMonitorData();
+
+    return {
+      props: {
+        monitorStats
+      },
+      revalidate: 60,
+    }
+  } catch (e : any) {
+    return {
+      props: {
+        loadingError: e?.message
+      },
+      revalidate: 1,
+    }
+  }
 }
 
 export default Home
